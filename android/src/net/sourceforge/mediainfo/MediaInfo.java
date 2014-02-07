ed43c22d9d63866638fba74dd4d17cb591733b9e
Andrew Jang <jaeguly@gmail.com>
2014-02-07 13:19:24 +0900

    Added main methods of MediaInfo with detail comments.

--- android/src/net/sourceforge/mediainfo/MediaInfo.java
+++ android/src/net/sourceforge/mediainfo/MediaInfo.java
@@ -44,6 +44,83 @@ public class MediaInfo {
         }
     }
 
+    /**
+     * Open a file and collect information about it (technical information and tags)
+     * @param finename Full name of file to open
+     * @return 0 file not opened
+     *         1 file opened
+     */
+    public int open(String filename) {
+        return open(peer, filename);
+    }
+
+    /** Close a file opened before with open(). */
+    public void close() {
+        close(peer);
+    }
+
+    /**
+     * Get a piece of information about a file. (parameter is an integer)
+     * @param streamKind Kind of Stream
+     * @param streamNum Stream number in Kind of stream
+     * @param parameter Parameter you are looking for in the stream (codec, width, bitrate, ..),
+     *                  in integer format
+     * @param infoKind Kind of information you want about the parameter (the text, the measure,
+     *                 the help, ..)
+     * @return a string about information you search
+     *         an empty string if there is a problem
+     */
+    public String get(StreamKind streamKind, int streamNum, int parameter) {
+        return getById(peer, streamKind.ordinal(), streamNum, parameter); /* InfoKind.TEXT */
+    }
+
+    /**
+     * Get a piece of information about a file. (parameter is an string)
+     * @param streamKind Kind of Stream (general, video, audio, ..)
+     * @param streamNum Stream number in Kind of stream
+     * @param parameter Parameter you are looking for in the stream (codec, width, bitrate, ..),
+     *                  in string format ("Codec", "Width", ..)
+     * @param infoKind Kind of information you want about the parameter (the text, the measure,
+     *                 the help, ..)
+     * @param searchKind Where to look for the parameter
+     * @return a string about information you search
+     *         an empty string if there is a problem
+     * @see option("Info_Parameters") to have the full list for @p parameter
+     */
+    public String get(StreamKind streamKind, int streamNum, String parameter) {
+        return getByName(peer, streamKind.ordinal(), streamNum, parameter); /*InfoKind.TEXT, InfoKind.NAME*/
+    }
+
+    public String get(StreamKind streamKind, int streamNum, String parameter, InfoKind infoKind) {
+        return getByNameDetail(peer, streamKind.ordinal(), streamNum, parameter, infoKind.ordinal(), InfoKind.NAME.ordinal());
+    }
+
+    public String get(StreamKind streamKind, int streamNum, String parameter, InfoKind infoKind,
+            InfoKind searchKind) {
+        return getByNameDetail(peer, streamKind.ordinal(), streamNum, parameter, infoKind.ordinal(), searchKind.ordinal());
+    }
+
+//    /**
+//     * Configuration or get information about MediaInfoLib
+//     * @param name The name of option
+//     * @param value The value of option
+//     * @return Depend of the option: "" by default means No, other means Yes
+//     */
+//    public String getOption(String name) {
+//        return getOption(peer, name);
+//    }
+
+//    /** Count of streams of a stream kind. */
+//    public int count(StreamKind streamKind) {
+//        return count(peer, streamKind.ordinal(), -1);
+//    }
+
+//    /** Count of piece of information in this stream. */
+//    public int count(StreamKind streamKind, int streamNum) {
+//        return count(peer, streamKind.ordinal(), streamNum);
+//    }
+
+
     //
     // private, protected, static
     //
