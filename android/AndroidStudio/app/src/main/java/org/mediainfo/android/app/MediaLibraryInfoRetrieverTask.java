c86bf76480aedda03371c317e1e0fae612e8bf65
Andrew Jang <jaeguly@gmail.com>
2014-05-27 11:30:09 +0900

    missing java file

---
+++ android/AndroidStudio/app/src/main/java/org/mediainfo/android/app/MediaLibraryInfoRetrieverTask.java
@@ -0,0 +1,34 @@
+package org.mediainfo.android.app;
+
+import android.widget.TextView;
+
+import org.mediainfo.android.MediaInfo;
+
+/**
+ * Asynchronous task that retrieves a media info from a given file.
+ */
+public class MediaLibraryInfoRetrieverTask extends MediaInfoRetrieverTask {
+
+    public MediaLibraryInfoRetrieverTask(TextView textView) {
+        super(textView);
+    }
+
+    @Override
+    /** Override this method to perform a computation on a background thread. */
+    protected Void doInBackground(String... paths) {
+
+        // Info about the library
+        publishProgress(MediaInfo.optionStatic("Info_Version"));
+
+        publishProgress("\n\n\n### Parameters\n\n");
+        publishProgress(MediaInfo.optionStatic("Info_Parameters"));
+
+        publishProgress("\n\n\n### Codecs\n\n");
+        publishProgress(MediaInfo.optionStatic("Info_Codecs"));
+
+        publishProgress("\n\n\n### Capacities\n\n");
+        publishProgress(MediaInfo.optionStatic("Info_Capacities"));
+
+        return null;
+    }
+}
